b51fd08bb4250a14c48a6c29ffb80b8b70d74dbf
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-22 07:52:57 -0600

    se realizo un formato a la salida

--- Actividad3/ejercicio1.py
+++ Actividad3/ejercicio1.py
@@ -3,7 +3,7 @@ calificacion2 = int(input("Ingrese la calificación del segundo examen: "))
 calificacion3 = int(input("Ingrese la calificación del tercer examen: "))
 suma = calificacion1 + calificacion2 + calificacion3
 promedio = suma / 3
-print(f"""calificacion 1: {calificacion1}
-calificacion 2: {calificacion2}
-calificacion 3: {calificacion3}
-promedio: {promedio:.0f}""")
+print(f"""calificacion 1: {calificacion1:>3}
+calificacion 2: {calificacion2:>3}
+calificacion 3: {calificacion3:>3}
+{"Promedio":<10}: {promedio:>3.0f}""")
