c6faa3c8505ff95afdb9ebdd4c19e1b7923f582e
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-25 08:24:21 -0600

    nuevo archivo

---
+++ Viernes/ejercicio3.py
@@ -0,0 +1,17 @@
+díadesemana = int(input("Introduce un número del 1 al 7: "))
+if díadesemana == 1:
+    print("Lunes")
+elif díadesemana == 2:
+    print("Martes")
+elif díadesemana == 3:
+    print("Miércoles")
+elif díadesemana == 4:
+    print("Jueves")
+elif díadesemana == 5:
+    print("Viernes")
+elif díadesemana == 6:
+    print("Sábado")
+elif díadesemana == 7:
+    print("Domingo")
+else:
+    print("Se produjo un error")
---
+++ Viernes/ejercicio4.py
@@ -0,0 +1,13 @@
+#Función
+def es_triangulo (lado1, lado2, lado3):
+    suma = lado1 , lado2
+    if (suma > lado3)
+        return "Es un triángulo"
+    else: 
+        return "No es un triángulo"
+    def main ():
+        lado1 = float(input("Introduce el primer lado: "))
+        lado2 = float(input("Introduce el segundo lado: "))
+        lado3 = float(input("Introduce el tercer lado: "))
+        resultado = es_triangulo(lado1, lado2, lado3)
+        print(resultado)
