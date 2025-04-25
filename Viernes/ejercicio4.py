bb857752c85116d138b6ded2c1ca7e2297c478dd
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-25 08:37:15 -0600

    se realizo un archivo a la salida

--- Viernes/ejercicio4.py
+++ Viernes/ejercicio4.py
@@ -1,13 +1,14 @@
 #Función
-def es_triangulo (lado1, lado2, lado3):
-    suma = lado1 , lado2
-    if (suma > lado3)
+def es_triangulo(lado1, lado2, lado3):
+    suma = lado1 + lado2
+    if (suma > lado3):
         return "Es un triángulo"
     else: 
         return "No es un triángulo"
-    def main ():
-        lado1 = float(input("Introduce el primer lado: "))
-        lado2 = float(input("Introduce el segundo lado: "))
-        lado3 = float(input("Introduce el tercer lado: "))
+def main():
+        lado1 = float(input("Ingrese el lado 1: "))
+        lado2 = float(input("Ingrese el lado 2: "))
+        lado3 = float(input("Ingrese el lado 3: "))
         resultado = es_triangulo(lado1, lado2, lado3)
-        print(resultado)
+        print(resultado)    
+main()
