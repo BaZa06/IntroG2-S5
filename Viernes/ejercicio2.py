6f5c3c4ae875a8fdb35d22579b234f461b8be0e6
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-25 07:56:53 -0600

    nuevo archivo

--- Viernes/ejercicio2.py
+++ Viernes/ejercicio2.py
@@ -2,9 +2,9 @@ sueldo = float(input("¿Cuál es su sueldo? "))
 bono = 0
 if sueldo > 3000:
     bono = sueldo * 0.10
-    print('Su bono es de:', bono)
+    print(f"Su bono es de: , {bono:.2f}")
 elif sueldo > 1500 and sueldo <= 3000:
     bono = sueldo * 0.05
-    print ('Su bono es de:', bono)
+    print (f"Su bono es de: , {bono:.2f}")
 elif sueldo <= 1500:
-    print('No tiene bono')
+    print("No tiene bono")
