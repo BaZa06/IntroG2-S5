47937b3e935e927e34f0d7cf1f50f8cdf208f3e5
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-24 23:05:13 -0600

    Nuevo archivo

---
+++ Actividad4/ejercicio1.py
@@ -0,0 +1,8 @@
+inicio = int(input("Dime el primer valor: "))
+aleta = int(input("Dime el último valor: "))
+
+número = int(input("Ingrese un número: "))
+if número >= inicio and número <= aleta:
+    print("El número es válido.")
+else:
+    print("El número no es válido.")
